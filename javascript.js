7967a4e106d160eb939210ef23f8d85249148a62
Mikki Janower <info@mikki.studio>
2025-07-18 21:26:15 +0100

    top bar on all windows

--- javascript.js
+++ javascript.js
@@ -1,9 +1,13 @@
 $(document).ready(function () {
 
 
-/*--------------make '.draggable' elements draggable-------------*/
+/*--------------make terms draggable n resizable-------------*/
 $( ".termy" ).draggable().resizable();
 
+/*--------------append header to all terms---------------*/
+$(".status-bar").append(
+    "<span style='background: var(--red);'></span> <span style='background: var(--yellow)';></span> <span style='background: var(--green);'></span>"
+);
 
   //-----------------------------document closing bracket; don't touch
 });
