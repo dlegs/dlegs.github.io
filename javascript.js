f77c7007cc8bdd76c8da66528e7ebf5012395e83
Mikki Janower <info@mikki.studio>
2025-08-01 00:12:49 +0100

    -webkit-fill-available fix to ensure things fit mobile and ipad viewports

--- javascript.js
+++ javascript.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+/*window.onresize = function() {
+    document.body.height = window.innerHeight;
+}
+window.onresize(); // called to initially set the height.*/
 /*---------------initialize terminal---------------*/
 $('#terminal').terminal(
   function(command, term) {
